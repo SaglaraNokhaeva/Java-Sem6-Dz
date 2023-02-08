053acb1b33c8a56b271c6de93d90a79e8c36dd58
Saglara <nohaevass@yandex.ru>
2023-02-08 21:54:17 +0300

    Create Notebook.java

---
+++ Notebook.java
@@ -0,0 +1,11 @@
+
+public class Notebook {
+    public Notebook(int i, int j, String string, String string2) {
+    }
+    Integer ram_memory;
+    Integer hard_drive;
+    String os;
+    String colour;
+
+    
+}
