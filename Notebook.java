15bd5f940c451055969f1c541eedd68c54f7e103
Saglara <nohaevass@yandex.ru>
2023-02-08 22:09:46 +0300

    Update Notebook.java

--- Notebook.java
+++ Notebook.java
@@ -2,10 +2,30 @@
 public class Notebook {
     public Notebook(int i, int j, String string, String string2) {
     }
+
     Integer ram_memory;
     Integer hard_drive;
     String os;
     String colour;
 
-    
+    public Notebook(int i, int j, String string1, String string2) {
+        this.ram_memory = i;
+        this.hard_drive = j;
+        this.os = string1;
+        this.colour = string2;
+
+
+    // this.registered = true;
+    // this.nameMaster = master;
+    // this.telephone = getPhone;
+    // this.name = petName;
+    // this.sex = petSex;
+    // this.breed = petBreed;
+    // this.age = petAge;
+    }
+
+    public Integer get_ram_memory() {
+        return this.ram_memory;
+    }
+
 }
